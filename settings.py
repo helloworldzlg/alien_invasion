da0b857b5e6dd2fa1b750b628c089c3e2336d524
zhanglg <zhanglg@partnerX.com>
2018-02-28 16:59:00 +0800

    Game v0.1

---
+++ settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -- coding: utf-8 --
+
+class Settings():
+
+    def __init__(self):
+        self.screen_width  = 1200
+        self.screen_height = 800
+        self.bg_color      = (230, 230, 230)
